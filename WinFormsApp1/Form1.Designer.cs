c80c513270a0ea49fc0673eabfff57f9eeb81188
minagergesw <minagerges3020@gmail.com>
2021-04-13 04:50:13 +0200

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tajawal", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(360, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "الجذر التربيعى";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Tajawal", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(360, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "مربع الرقم";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Font = new System.Drawing.Font("Tajawal", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt1.Location = new System.Drawing.Point(360, 60);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(196, 31);
+            this.txt1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tajawal", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(430, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "؟؟؟";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txt2
+            // 
+            this.txt2.Font = new System.Drawing.Font("Tajawal", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt2.Location = new System.Drawing.Point(42, 56);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(175, 31);
+            this.txt2.TabIndex = 5;
+            this.txt2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txt2.TextChanged += new System.EventHandler(this.txt2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tajawal", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(420, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "رقم واحد";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tajawal", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(142, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "رقمين";
+            // 
+            // txt3
+            // 
+            this.txt3.Font = new System.Drawing.Font("Tajawal", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt3.Location = new System.Drawing.Point(42, 90);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(175, 31);
+            this.txt3.TabIndex = 8;
+            this.txt3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Tajawal", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(42, 131);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 43);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "طرح";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Tajawal", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(178, 180);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 43);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "ضرب";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Tajawal", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(178, 131);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(125, 43);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "جمع";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Tajawal", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(42, 180);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(130, 43);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "قسمة";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Tajawal", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(420, 208);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 29);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "الناتج";
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Tajawal", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button7.Location = new System.Drawing.Point(42, 229);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(261, 48);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "باقى القسمة";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tajawal", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(223, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 23);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "الرقم الاول";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Tajawal", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(218, 93);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 23);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "الرقم الثانى";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 306);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        double num = Convert.ToDouble(txt1.Text);
+            label2.Text =Convert.ToString( Math.Sqrt(num));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double num = Convert.ToDouble(txt1.Text);
+            label2.Text = Convert.ToString(num*num);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(txt2.Text);
+            double num2 = Convert.ToDouble(txt3.Text);
+            label2.Text = Convert.ToString(num1/ num2);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(txt2.Text);
+            double num2 = Convert.ToDouble(txt3.Text);
+            label2.Text = Convert.ToString(num1 + num2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(txt2.Text);
+            double num2 = Convert.ToDouble(txt3.Text);
+            label2.Text = Convert.ToString(num1 -num2);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(txt2.Text);
+            double num2 = Convert.ToDouble(txt3.Text);
+            label2.Text = Convert.ToString(num1 * num2);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(txt2.Text);
+            double num2 = Convert.ToDouble(txt3.Text);
+            double result= num1 % num2;
+            if (num1<num2) {
+                result = 0;
+            }
+
+            label2.Text = Convert.ToString(result);
+          
+        }
+
+        private void txt2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
